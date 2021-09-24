a8e61ed80f1ae001db60bd55499070f14ee2d655
xcloudyunx <46149323+xcloudyunx@users.noreply.github.com>
2021-09-24 22:09:46 +1200

    Renamed

--- android/app/src/main/java/com/tapcontrol/MainActivity.java
+++ android/app/src/main/java/com/tapcontrol/MainActivity.java
@@ -1,4 +1,4 @@
-package com.remote;
+package com.tapcontrol;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "remote";
+    return "TapControl";
   }
 }
